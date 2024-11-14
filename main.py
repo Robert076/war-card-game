13e03ec1133402f30f3453f6afd1aea93d174895
Robert076 <robertberes06@gmail.com>
2024-11-14 11:11:56 +0200

    deck class

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import random
+
+
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 
 class Card:
     def __init__(self, suit, rank):
@@ -9,6 +14,19 @@ class Card:
     def __str__(self):
         return "Card(" + self.rank + " of " + self.suit + ")"
 
+class Deck:
+    def __init__(self):
+        self.allCards = []
+        for suit in suits:
+            for rank in ranks:
+                self.allCards.append(Card(suit, rank))
+    
+    def shuffle(self):
+        random.shuffle(self.allCards)
 
-e = Card('Hearts', 'Two')
-print (e.value)
+
+
+newDeck = Deck()
+newDeck.shuffle()
+for card in newDeck.allCards:
+    print(card)
